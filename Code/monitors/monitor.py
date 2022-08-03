8f6873f338de7cb7d8648ecf907a7a94bf5299f7
HenriDeClercq <93086052+HenriDeClercq@users.noreply.github.com>
2022-08-03 15:35:37 +0200

    Add files via upload

---
+++ Code/monitors/monitor.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from pyparsing import col
+from filelock import FileLock, Timeout
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import time
+
+class Monitor:
+    def __init__(self, fileName, columnToPlot, horizontalWindowSize, sleepTime):
+            self.plotRefreshRate = 60
+            self.filelock = FileLock(fileName+".lock", timeout=1)
+            self.iteratorPosition = 0
+
+            self.setCOLUMNINDEX(columnToPlot)
+            self.legal_SF_PERIOD_VALUES = [1,2,4,8,16,32]
+            self.legal_SF_ALLOC_VALUES = [1,2,3,4,5,6]
+
+    	    # tweakable arguments
+            self.fileName = fileName
+            self.columnToPlot = columnToPlot
+            self.horizontalWindowSize = horizontalWindowSize
+            self.sleepTime = sleepTime
+
+    def run(self):
+        animation = FuncAnimation(plt.gcf(), self.drawNextFrame, self.plotRefreshRate)
+        plt.show()
+
+    def readCSV(self):
+        self.filelock.acquire()
+        try:
+            valuesOfAllColumns = pd.read_csv(self.fileName, skiprows = self.iteratorPosition, nrows = self.horizontalWindowSize)
+            valuesOfXaxis = valuesOfAllColumns.iloc[:, 0]
+            valuesOfDesiredColumn = valuesOfAllColumns.iloc[:, self.COLUMNINDEX]
+            self.nextIteratorPosition(valuesOfAllColumns)
+        finally:
+            self.filelock.release()
+        return valuesOfXaxis, valuesOfDesiredColumn
+
+    def nextIteratorPosition(self, valuesOfAllColumns):
+            if (valuesOfAllColumns.shape[0] == self.horizontalWindowSize):
+                self.iteratorPosition+=1
+    
+    def setVisualPlotParameters(self):
+        plt.cla()
+        plt.title('File: ' + self.fileName.split("/")[-1])
+
+    def fetchValuesToPlot(self):
+        return self.readCSV()
+
+    def getMaxLegalValue(self):
+        if (self.columnToPlot == "SF_ALLOC"):
+            maxValue = max(self.legal_SF_ALLOC_VALUES)
+        if (self.columnToPlot == "SF_PERIOD"):
+            maxValue = max(self.legal_SF_PERIOD_VALUES)
+        return maxValue
+
+    def setCOLUMNINDEX(self, columnToPlot):
+        if columnToPlot == "SF_ALLOC":
+            self.COLUMNINDEX = 1
+        else:
+            self.COLUMNINDEX = 2
+    
+    def drawNextFrame(self, i):
+        print("Drawing next frame")
+        time.sleep(self.sleepTime)
---
+++ Code/network_components/client.py
@@ -0,0 +1,63 @@
+import csv
+import time
+import random
+import argparse
+import socket
+from filelock import FileLock, Timeout
+
+class dataSender:
+    def __init__(self, addressOfServer, portOfServer, sleeptime, filename = "csv_files/random_generated.csv"):
+        self.filename = filename
+        self.filelockname = filename+".lock"
+        self.filelock = FileLock(self.filelockname, timeout=1)
+        self.addressOfServer = addressOfServer
+        self.portOfServer = portOfServer
+        self.server_address = (self.addressOfServer, self.portOfServer)
+        
+        self.sleeptime = sleeptime
+        self.legal_SF_ALLOC = [1,2,3,4,5,6]
+        self.legal_SF_PERIOD = [1,2,4,8,16,32]
+        
+    def run(self):
+        while True:
+            SF_ALLOC, SF_PERIOD = self.generateNewRandomFrame()
+            newRowToAppend = [SF_ALLOC, SF_PERIOD]
+            self.sendNewRowToServer(newRowToAppend)
+            time.sleep(self.sleeptime)
+
+    def sendNewRowToServer(self, newRowToAppend):
+        newSocket = self.initiateSocket()
+        #print("Connection to server on %s port %s" % self.server_address)
+        newSocket.connect(self.server_address)
+        try:
+            print("Sending: " + str(newRowToAppend))
+            bytesToSend = str.encode(str(newRowToAppend[0]) + "," + str(newRowToAppend[1]))
+            newSocket.sendall(bytesToSend)
+        finally:
+            #print("Closing connection...")
+            newSocket.close()
+
+    def generateNewRandomFrame(self):
+            SF_ALLOC = random.choice(self.legal_SF_ALLOC)
+            SF_PERIOD = random.choice(self.legal_SF_PERIOD)
+            return (SF_ALLOC, SF_PERIOD)
+
+    def initiateSocket(self):
+        newSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        return newSocket
+
+def main(addressOfServer, portOfServer, sleeptime):
+    generator = dataSender(addressOfServer, portOfServer, sleeptime)
+    generator.run()
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Create a random data generator client')
+    parser.add_argument('--addressOfServer', type=str, required=False, default= "localhost",
+                        help='time between sending new rowes')
+    parser.add_argument('--portOfServer', type=int, required=False, default= 10000,
+                        help='time between sending new rowes')
+    parser.add_argument('--sleeptime', type=float, required=False, default= 0.5,
+                        help='time between sending new rowes')
+    args = parser.parse_args()
+    main(addressOfServer = args.addressOfServer, portOfServer = args.portOfServer, sleeptime = args.sleeptime)
+
