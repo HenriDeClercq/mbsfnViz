08ddaf4b2d9227138b44ef76cf29d24fb0ff065f
HenriDeClercq <93086052+HenriDeClercq@users.noreply.github.com>
2022-08-04 10:43:52 +0200

    Add files via upload

---
+++ Code/README.txt
@@ -0,0 +1,10 @@
+clientside: run Code/network_components/client.cpp or client.py to generate and send random values
+		 run the modified adaptive_mbsfn which sends the parameters to the server
+
+serverside: run Code/network_components/server.py --addressOfServer "insert" or just default "127.0.0.1" (same with client.py)
+		 run Code/monitors/main.py --monitorType "insert bardiagram or piechart" or just default "bardiagram"
+
+!!! MAKE SURE THAT THE IP OF THE SERVERADDRESS IS CONFIGURED CORRECTLY!!
+
+!!! MONITOR ADJUST SLEEPTIME FOR FRAMERATE => 0 MEANS ASAP
+
---
+++ Code/monitors/main.py
@@ -0,0 +1,29 @@
+import argparse
+import type_bardiagram as bar
+import type_piechart as pie
+
+def main(monitorType, fileName, columnToPlot, horizontalWindowSize, sleepTime):
+    if monitorType == "bardiagram":
+        monitor = bar.Monitor(fileName, columnToPlot, horizontalWindowSize, sleepTime)
+    elif monitorType == "piechart":
+        monitor = pie.Monitor(fileName, columnToPlot, sleepTime)
+    else:
+        print("WRONG MONITORTYPE")
+        return
+    monitor.run()
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Create a type of Monitor')
+    
+    parser.add_argument('--monitorType', type=str, required=False, default= "bardiagram",
+                        help='choose: "piechart" or "bardiagram"')
+    parser.add_argument('--filename', type=str, required=False, default= "random_generated.csv",
+                        help='the path to the csv filename')
+    parser.add_argument('--columnToPlot', type = str, required=False, default = "SF_ALLOC", 
+                        help='choose: "SF_ALLOC" or "SF_PERIOD"')
+    parser.add_argument('--horizontalWindowSize', type = int, required=False, default = 20,
+                        help='the width of the window x axis')
+    parser.add_argument('--sleepTime', type = float, required=False, default = 0.1,
+                        help='time to sleep between frames')
+    args = parser.parse_args()
+    main(monitorType = args.monitorType, fileName=args.fileName, columnToPlot=args.columnToPlot, horizontalWindowSize = args.horizontalWindowSize, sleepTime = args.sleepTime) 
