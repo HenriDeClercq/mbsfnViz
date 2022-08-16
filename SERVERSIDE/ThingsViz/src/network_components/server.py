56ce5f5f31f21b8aabad7f970097e7d5b6dbbb9d
HenriDeClercq <93086052+HenriDeClercq@users.noreply.github.com>
2022-08-16 12:13:51 +0200

    Final Version

---
+++ SERVERSIDE/ThingsViz/src/network_components/server.py
@@ -0,0 +1,23 @@
+import functions_socket as socket
+import functions_mqtt as mqtt
+#INFO: Listens for messages recieved from client and publishes them to the mqtt broker.
+
+class Server:
+    def __init__(self, addressOfBroker, portOfBroker, deviceAccessToken, topicGroup, addressOfServer, portOfServer, sleeptime):
+        # MQTT BROKER VALUES
+        self.addressOfBroker = addressOfBroker
+        self.portOfBroker = portOfBroker  
+        self.topicGroup = topicGroup
+        self.username = deviceAccessToken
+        self.password = "nopassword"
+        self.clientID = "THISISTHEUNIQUESERVERID"   
+
+        # SOCKET VALUES
+        self.addressOfServer = addressOfServer
+        self.portOfServer = portOfServer
+        self.socket = socket.initiateSocket(self.addressOfServer, self.portOfServer)
+        
+    def run(self):
+        while True:
+            newParametersToSend = socket.listenForNewMessage(self.socket)
+            mqtt.sendNewParametersToBroker(newParametersToSend, self.clientID, self.username, self.password, self.addressOfBroker, self.portOfBroker, self.topicGroup)
