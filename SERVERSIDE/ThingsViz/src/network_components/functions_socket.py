41b7b0812d83ce1b9254f5fd4555e0f78f48e81f
HenriDeClercq <93086052+HenriDeClercq@users.noreply.github.com>
2022-08-18 10:14:48 +0200

    Final Version

---
+++ SERVERSIDE/ThingsViz/src/network_components/functions_socket.py
@@ -0,0 +1,28 @@
+import socket
+
+def listenForNewMessage(Socket):
+    newConnection = listenForAndAcceptNewConnection(Socket)
+    try:
+        SF_ALLOC, SF_PERIOD, PERCENTAGE = recieveMessageFromConnectedClient(newConnection)
+    finally:
+        newConnection.close()
+        return SF_ALLOC, SF_PERIOD, PERCENTAGE
+
+def listenForAndAcceptNewConnection(Socket):
+    Socket.listen(1)
+    connection, client_address = Socket.accept() 
+    return connection
+
+def recieveMessageFromConnectedClient(connection):
+    dataInBytes = connection.recv(16)
+    dataInString = dataInBytes.decode()
+    SF_ALLOC, SF_PERIOD, PERCENTAGE = dataInString.split(",")
+    print("Received: " + dataInString)
+    return int(SF_ALLOC), int(SF_PERIOD), int(PERCENTAGE)
+        
+def initiateSocket(addressOfServer, portOfServer):
+    newSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = (addressOfServer, portOfServer)
+    print("Starting up the server on %s port %s" % server_address)
+    newSocket.bind(server_address)
+    return newSocket
